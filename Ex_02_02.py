85f47f409dfb1632e3db2bbb4ce65baf28b2dfe0
Valery Lapeza <valery_lapeza@epam.com>
2023-03-25 13:49:09 +0300

    new changes

--- Ex_02_02.py
+++ Ex_02_02.py
@@ -25,6 +25,12 @@ print(hours * rate)
 # width//2
 # width/2.0
 # height/3
-# 1 + 2 \* 5
+# 1 + 2 * 5
 
+width = 17
+height = 12.0
 
+print(type(width//2))
+print(type(width/2.0))
+print(type(height/3))
+print(type(1 + 2 * 5))
