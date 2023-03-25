218fca8f3904ae4b979b073be3b81a24c0fc845e
Valery Lapeza <valery_lapeza@epam.com>
2023-03-25 13:12:52 +0300

    learning Python

---
+++ Ex_02_02.py
@@ -0,0 +1,30 @@
+# Write a program that uses input to prompt a user for their name and then welcomes them
+# Enter your name: Chuck
+# Hello Chuck
+
+name = input('Enter your name: ')
+print('Hello', name)
+
+
+# Write a program to prompt the user for hours and rate per hour to compute gross pay
+# Enter Hours: 35
+# Enter Rate: 2.75
+# Pay: 96.25
+
+hours = int(input('Enter Hours: '))
+rate = float(input('Enter Rate: '))
+print(hours * rate)
+
+
+# Assume that we execute the following assignment statements:
+#
+# width = 17
+# height = 12.0
+# For each of the following expressions, write the value of the expression and the type (of the value of the expression)
+#
+# width//2
+# width/2.0
+# height/3
+# 1 + 2 \* 5
+
+
